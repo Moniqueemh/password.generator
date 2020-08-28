7dc4ffd1c7748b8ce9c4bc0889eeb20d6587d60e
Moniqueemh <mohernandez9992@yahoo.com>
2020-08-27 21:17:32 -0700

    confirms and creating characters

--- Develop/script.js
+++ Develop/script.js
@@ -16,10 +16,32 @@ function writePassword() {
 /**
  * generate a password based on certain criteria
  */
+
 function generatePassword(){
+  
+  let lowercaseLetters =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  let uppercaseLetters =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  let numbers =[0,1,2,3,4,5,6,7,8,9];
+  let specialCharacters =['!','@','#','$','%','^','&','*','(',')','-','_','=','+','<','>','/','?'];
+
+  
+
+
 
   // Password is currently blank! We need to make a better one
   let password = "";
+
+  let passwordLength = prompt("How long do you want your password to be?");
+  
+  let confirmUppercase = confirm("Do you want to use upper case letters?");
+  let confirmLowercase = confirm("Do you want to use lower case letters?");
+  let confirmNumbers = confirm("Do you want to use numbers?");
+  let confirmSpecialCharacters = confirm("Do you want to use special characters?");
+
+  console.log (passwordLength, confirmUppercase, confirmLowercase, confirmNumbers, confirmSpecialCharacters);
+
+  
+
 
 
 
